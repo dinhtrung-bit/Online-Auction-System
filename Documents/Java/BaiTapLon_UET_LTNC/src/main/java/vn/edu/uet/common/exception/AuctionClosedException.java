4b036708978f41a519459dc3efa70507ceb34a1f
Đình Trung <25023417@vnu.edu.vn>
2026-03-22 15:36:09 +0700

    b1

--- Documents/Java/BaiTapLon_UET_LTNC/src/main/java/vn/edu/uet/common/exception/AuctionClosedException.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.uet.common.exception;
-
-public class AuctionClosedException extends RuntimeException {
-    public AuctionClosedException(String message) {
-        super(message);
-    }
-}
--- Documents/Java/BaiTapLon_UET_LTNC/src/main/java/vn/edu/uet/common/model/AuctionStatus.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.uet.common.model;
-
-public enum AuctionStatus {
-    OPEN,
-    RUNNING,
-    FINISHED,
-    PAID,
-    CANCELED
-}
