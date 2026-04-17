ed2e5cfb189caf64be9bac7a2f28a3e9b8cf4865
Tran Thanh <ichinoseharu404@gmail.com>
2026-04-17 20:57:17 +0700

    p1

---
+++ auction-server/src/server/networks/ClientHandler.java
@@ -0,0 +1,19 @@
+package server.networks;
+
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket clientSocket;    // tạo phương thức truyền dữ liệu từ client đi
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override               // override hàm run của Runnable
+    public void run() {     // cái này để chạy th.start() ở file bên kia
+        try {
+            System.out.println("Dang xu ly: " + clientSocket.getInetAddress());     // lấy ip cuả client
+        } catch (Exception e) {
+            e.printStackTrace();    // in ra lỗi
+        }
+    }
+}
