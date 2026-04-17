0d69a79ee1f9dbe9a865c64b1679554fa49968db
toan287 <toan2872007@gmail.com>
2026-04-18 01:05:59 +0700

    Delete IBidService.java

--- auction-client/src/client/controllers/IBidService.java
+++
@@ -1,9 +0,0 @@
-package client.controllers;
-
-public interface IBidService {
-    // Hàm xử lý đặt giá chính thức
-    void processBid(int userId, int auctionId, double amount) throws Exception;
-
-    // Hàm cho chức năng nâng cao Auto-bid
-    void setupAutoBid(int userId, int auctionId, double maxBid, double increment);
-}
