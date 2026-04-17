1ab716454efe1666c40469ecec3fca3d2fee5a7f
Đình Trung <25023417@vnu.edu.vn>
2026-04-17 15:52:22 +0700

    fix code

--- auction-client/src/client/MainApp/Launcher.java
+++
@@ -1,7 +0,0 @@
-package client.MainApp; // Chú ý dòng package này phải khớp với thư mục của bạn
-
-public class Launcher {
-    public static void main(String[] args) {
-        ClientApp.main(args);
-    }
-}
