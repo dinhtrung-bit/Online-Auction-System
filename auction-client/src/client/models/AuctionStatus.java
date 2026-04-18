4800439fbd1b9a17b5e0b182a2caa4d3a83b08b9
Đình Trung <25023417@vnu.edu.vn>
2026-04-18 21:52:22 +0700

    fixx

---
+++ auction-client/src/client/models/AuctionStatus.java
@@ -0,0 +1,10 @@
+package client.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+// Enum trạng thái phòng
+public enum AuctionStatus {
+    OPEN, RUNNING, FINISHED, CANCELED
+}
+
