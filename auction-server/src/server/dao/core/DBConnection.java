bfe3e3581d26fdc5c73a05400de4e40433dc31d8
Tran Thanh <ichinoseharu404@gmail.com>
2026-05-06 21:50:54 +0700

    update dbconnection

--- auction-server/src/server/dao/core/DBConnection.java
+++ auction-server/src/server/dao/core/DBConnection.java
@@ -5,20 +5,36 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/daugia?useSSL=false&serverTimezone=Asia/Ho_Chi_Minh&allowPublicKeyRetrieval=true";
+    private static DBConnection instance;
+    private static Connection connection;
+    private static final String URL = "jdbc:mysql://localhost:3306/daugia";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
-    static {
+    private DBConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");//tạo ra cầu nối giữa java và csdl
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Kết nối cơ sở dữ liệu thành công");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Không tìm thấy MySQL Driver!", e);
+        }
+        catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException("Lỗi kết nối cơ sở dữ liệu");
         }
     }
+    public static synchronized DBConnection getInstance() {
+        try {
+            if (instance == null || instance.getConnection().isClosed()) {
+                instance = new DBConnection();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return instance;
+    }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() {
+        return connection;
     }
 }
+
